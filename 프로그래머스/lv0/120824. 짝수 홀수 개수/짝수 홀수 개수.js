ffcc03a7c9649c62f0d2dc7b94fc2d4305c6279b
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-07-16 17:12:01 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,5 +1,5 @@
 function solution(num_list) {
-    var even = num_list.filter(i => i % 2 === 0);
-    var odd = num_list.filter(i => i % 2 !== 0);
+    const even = num_list.filter(i => i % 2 === 0);
+    const odd = num_list.filter(i => i % 2 !== 0);
     return [even.length, odd.length];
 }
