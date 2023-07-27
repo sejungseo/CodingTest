d2b91135c93c4f420c652eda70b0581d9f527fa0
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-07-27 22:32:17 +0900

    [unrated] Title: 수 조작하기 1, Time: 6.00 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    var answer = n;
+    for(i = 0; i < control.length; i++) {
+        if (control[i] == "w") {
+            answer += 1;
+        } if (control[i] == "s") {
+            answer -= 1;
+        } if (control[i] == "d") {
+            answer += 10;
+        } if (control[i] == "a") {
+            answer -= 10;
+        }
+    }
+    return answer;
+}
