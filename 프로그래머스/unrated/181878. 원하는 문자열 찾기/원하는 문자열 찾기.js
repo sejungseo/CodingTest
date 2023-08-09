1215442cc4b49f6d7c4372811e9620148a5b2efe
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-09 21:13:57 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,5 +1,3 @@
 function solution(myString, pat) {
-    let newstr = myString.toLowerCase();
-    let newpat = pat.toLowerCase();
-    return newstr.includes(newpat) ? 1 : 0;
+    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
 }
