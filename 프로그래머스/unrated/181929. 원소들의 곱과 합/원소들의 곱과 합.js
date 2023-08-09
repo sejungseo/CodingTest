1c709f91b50593e1ee379060cba8728de7eb62a0
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-09 20:52:24 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,5 +1,5 @@
 function solution(num_list) {
-    let multifly = num_list.reduce((a, b) => a * b);
-    let sum = num_list.reduce((a,b) => a + b);
+    let multifly = num_list.reduce((a, b) => a * b, 1);
+    let sum = num_list.reduce((a,b) => a + b, 0);
     return multifly < sum ** 2 ? 1 : 0;
 }
