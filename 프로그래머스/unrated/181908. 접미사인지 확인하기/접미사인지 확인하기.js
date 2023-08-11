4e7be1047e23899a25551f45feec289d68eb255d
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-11 18:28:25 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,1 @@
-function solution(my_string, is_suffix) {
-    return my_string.endsWith(is_suffix) ? 1 : 0;
-}
+ const solution = (my_string, is_suffix) => my_string.slice(my_string.length - is_suffix.length) === is_suffix? 1:0
