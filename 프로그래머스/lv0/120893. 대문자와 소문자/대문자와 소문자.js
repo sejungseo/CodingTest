65105620b43d5f5a0803084a2608a6203a556780
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-13 21:24:16 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
-    var answer = '';
+    let answer = '';
     for(let i = 0; i < my_string.length; i++) {
         if(my_string[i] === my_string[i].toUpperCase()){
             answer += my_string[i].toLowerCase();
