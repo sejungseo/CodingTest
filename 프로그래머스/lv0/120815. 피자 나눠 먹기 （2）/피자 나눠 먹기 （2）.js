b7a3c0ab1fc6f9cbe684b315882d5d29eea46628
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-19 16:06:59 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,11 @@
+
+function solution(n) {
+    let answer = 0;
+    for(let i = Math.max(n, 6); i <= n * 6; i++){
+        if(i % n === 0 && i % 6 === 0) {
+            answer = i;
+            break;
+        }
+    }
+    return Math.floor(answer / 6);
+}
