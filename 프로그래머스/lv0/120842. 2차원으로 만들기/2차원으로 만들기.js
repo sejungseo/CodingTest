281fe912e67510bca3f6a119934fe7fe930df8e2
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-26 23:43:35 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,6 +1,6 @@
 function solution(num_list, n) {
-    const answer = [];
-    while(num_list.length) {
+    let answer = [];
+    while (num_list.length) {
         answer.push(num_list.splice(0, n));
     }
     return answer;
