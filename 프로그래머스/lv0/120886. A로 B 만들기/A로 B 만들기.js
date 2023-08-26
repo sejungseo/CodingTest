014eae35e2ab1e80d173e02a6d7180e2f97284ca
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-27 01:13:17 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,3 +1,3 @@
 function solution(before, after) {
-    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
+    return [...before].sort().join('') === [...after].sort().join('') ? 1 : 0;
 }
