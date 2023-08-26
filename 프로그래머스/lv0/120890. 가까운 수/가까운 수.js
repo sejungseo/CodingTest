d606d07b616b928686caa3c93148743f051d0744
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-27 02:12:55 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,15 +1,15 @@
 function solution(array, n) {
-    let sortedArr = array.sort();
-    let absArr = [];
-    let minNum = 0
+    let sortedArr = array.sort(); // 가장 가까운 수가 여러 개일 경우 더 작은 수를 return하므로 array 정렬
+    let absArr = []; // 절대값
+    let minNum = 0 // 절대값 중 가장 작은 값
     for(let i = 0; i < sortedArr.length; i++){
         if(sortedArr[i] === n) {
             return n;
         } else {
-            absArr.push(Math.abs(sortedArr[i] - n));
+            absArr.push(Math.abs(sortedArr[i] - n)); // Math.abs() 절대값 반환
         }
         for(let j = 0; j < absArr.length; j++){
-            minNum = Math.min(...absArr);
+            minNum = Math.min(...absArr); // 절대값이 가장 작은 값 확인
         }
     }
     return sortedArr[absArr.indexOf(minNum)];
