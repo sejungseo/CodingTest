47fcb6e47a07088be68f81011d4f7223c73f92cf
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-08-27 02:13:39 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,4 @@
 function solution(emergency) {
-    let order = emergency.slice().sort((a, b) => b - a);
+    let order = emergency.slice().sort((a, b) => b - a); // slice() => 원본 배열 바뀌지 않고 복사본 생성
     return emergency.map(v => order.indexOf(v)+1);
 }
