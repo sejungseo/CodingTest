1327870a8435f0bc072652b42b6d0f128789abbd
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-09-05 03:08:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,3 +1,5 @@
+let numbers = "onefouronesixseven"
+
 function solution(numbers) {
     let num_str = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     let num_char = ["0","1","2","3","4","5","6","7","8","9"];
@@ -6,5 +8,5 @@ function solution(numbers) {
         numbers = numbers.replaceAll(num_str[i], num_char[i]);
     }
     
-    return parseInt(numbers);
+    return Number(numbers);
 }
