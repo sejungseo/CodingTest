a1d6f041e5babeb8bdf05a48762f3dca49052496
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-09-07 22:59:42 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -5,8 +5,10 @@ function solution(my_string) {
         if(arr[i] === '+') {
             answer += Number(arr[i+1]); 
         } else if(arr[i] === '-') {
-            answer -= Number(arr[i+1]);
+            answer -= Number(arr[i+1]); 
         } 
     }
-    return answer + Number(arr[0]);
+    return answer + Number(arr[0]); 
 }
+
+// 문자열로 된 연산식은 eval()로 계산할 수 있지만 해커가 위험한 코드를 사용할 수 있습니다. eval을 절대 사용하지 말 것!
