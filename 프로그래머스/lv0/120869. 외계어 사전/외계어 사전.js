03366c09426c7490cdeedfdd05f46187212c7c4c
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-09-09 14:21:38 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,5 +1,6 @@
 function solution(spell, dic) {
-    spell = spell.sort().join('');
-    dic = dic.map(v => v.split('').sort().join(''));
+    spell = spell.sort().join(''); // 문자열 정렬
+    dic = dic.map(v => v.split('').sort().join('')); // 배열의 각 요소 정렬
     return dic.includes(spell) ? 1 : 2;
 }
+// map => 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환
