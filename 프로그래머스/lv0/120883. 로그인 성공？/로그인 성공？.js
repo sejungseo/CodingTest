74bf62fbd5e6603551f9664dec6a384f8239fa10
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-09-19 19:45:32 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,5 +1,5 @@
 function solution(id_pw, db) {
-    var answer = '';
+    let answer = '';
     db.map(item => {if(id_pw[0] === item[0] && id_pw[1] === item[1]) answer='login'
                    else if(id_pw[0] === item[0] && id_pw[1] !== item[1]) answer='wrong pw'
                    else if(id_pw[0] !== item[0] && id_pw[1] !== item[1]) answer='fail'})
