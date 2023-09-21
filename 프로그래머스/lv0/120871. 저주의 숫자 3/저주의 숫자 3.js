4ba94c7d49db3d59ad8527b3fb4835338649c98a
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-09-21 22:36:22 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = 0;
+    
+    for(let i = 1; i <= n; i++) {
+        answer += 1; 
+        
+        while(answer % 3 === 0 || String(answer).includes('3')) { // 3의 배수이거나 3을 포함하는 수
+            answer++;
+        }
+    }
+    return answer;
+}
