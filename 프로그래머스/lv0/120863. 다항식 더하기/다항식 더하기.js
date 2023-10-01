051ab76c5a36b5e91f6321ff9e59705fad886003
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-10-01 15:28:14 +0900

    [level 0] Title: 다항식 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,12 +1,12 @@
 function solution(polynomial) {
-    const arr = polynomial.split(' + ');
-    let xnum = 0;
-    let num = 0;
+    const arr = polynomial.split(' + '); // ' + ' 기준으로 나누어 배열 arr 생성
+    let xnum = 0; // x의 수
+    let num = 0; // 정수의 수
     
     arr.forEach(i => {
         if(i.includes('x')) {
-            let xArr = i.split('x');  // x가 포함되어 있다면 x를 기준으로 split
-            if(xArr[0] === ''){
+            let xArr = i.split('x'); // x가 포함되어 있다면 x를 기준으로 나누어 xArr 생성
+            if(xArr[0] === ''){ 
                 xnum += 1;
             } else if(xArr[0] !== '') {
                 xnum += parseInt(xArr[0]);
