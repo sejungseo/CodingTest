b40636d34c2c67cf79269d2fb66e466d6b31ac08
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-10-01 14:39:50 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -2,7 +2,7 @@ function solution(A, B) {
     let arrA = [...A]; // 문자열A를 배열arrA로 변환
     for(let i = 0; i < arrA.length; i++) {
         if(A === B) 
-            return i;
+            return 0; 
         else {
         arrA.unshift(arrA.pop()); // arrA의 마지막 원소를 잘라내고 맨 앞에 붙이기
         if(arrA.join('') === B) // arrA를 문자열로 변환하고 문자열 B와 일치하는지 확인
