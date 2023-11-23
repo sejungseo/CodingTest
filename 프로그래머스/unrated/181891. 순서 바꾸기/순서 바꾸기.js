abde715f640246dd2b163c094e7e1a6274163f48
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-11-23 11:23:02 +0900

    [level unrated] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,5 +1,3 @@
 function solution(num_list, n) {
-    var answer = [];
-    let sliceList = num_list.splice(0, n);
-    return num_list.concat(sliceList);
+    return num_list.concat(num_list.splice(0, n));
 }
