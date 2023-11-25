5fabb55258c13481cc9549b7880f51f5ad5983c5
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-11-25 17:17:23 +0900

    [level unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,5 +1,5 @@
 function solution(str_list, ex) {
-    var answer = '';
+    let answer = '';
     for(let i = 0; i < str_list.length; i++) {
         if(!str_list[i].includes(ex)) {
             answer += str_list[i];
