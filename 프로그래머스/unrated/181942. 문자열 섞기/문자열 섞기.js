029a383fe40cd388267ea3bfcf9d00120d46737b
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-12-01 09:00:09 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,9 +1,7 @@
 function solution(str1, str2) {
     let answer = '';
-    let arr1 = str1.split('');
-    let arr2 = str2.split('');
     for(let i = 0; i < str1.length; i++) {
-        answer += arr1[i] + arr2[i]
+        answer += str1[i] + str2[i]
     }
     return answer;
 }
