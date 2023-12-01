91daa72a06e547ce9a17aa7be9c0d797b7feea39
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-12-01 10:07:00 +0900

    [level unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,11 @@
+function solution(arr, flag) {
+    let answer = [];
+    for(let i = 0; i < flag.length; i++) {
+        if(flag[i]) {
+            answer.push(...Array(arr[i] * 2).fill(arr[i]));
+        } else {
+            answer.splice(-arr[i]);
+        }
+    }
+    return answer;
+}
