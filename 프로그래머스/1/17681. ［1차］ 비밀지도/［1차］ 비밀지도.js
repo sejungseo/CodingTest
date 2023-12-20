cd8ac206fddab2dcacf7b282e39cf8a15c983e6a
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2023-12-20 09:49:37 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.08 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -2,6 +2,7 @@ function solution(n, arr1, arr2) {
     let answer = [];
     for(let i = 0; i < n; i++) {
         const strLine = (arr1[i] | arr2[i]).toString(2);
+        console.log(strLine)
         const arrLine = strLine.padStart(n, '0');
         const decLine = arrLine.replaceAll('0', ' ').replaceAll('1', '#');
         answer.push(decLine)
