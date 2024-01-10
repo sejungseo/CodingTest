c547bd2eb73c53b0421615ed228e0694a4c33adc
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-01-10 16:25:51 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,7 +1,5 @@
 function solution(arr) {
-    const min = Math.min(...arr);
-    arr.splice(arr.indexOf(min), 1)
-    return arr.length ? arr : [-1];
+  let answer = [];
+  answer = arr.filter(num => num > Math.min(...arr));
+  return answer.length ? answer : [-1];
 }
-
-// indexOf 배열에서 주어진 요소를 찾을 수 있는 첫 번째 인덱스를 반환
