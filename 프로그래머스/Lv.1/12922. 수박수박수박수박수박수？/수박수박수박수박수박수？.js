9d65d3a111a68aefb226fee256c1e42f1857a627
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-01-12 08:18:50 +0900

    [level Lv.1] Title: 수박수박수박수박수박수?, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/Lv.1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,10 @@
-function solution(n) {
-    let answer = '';
-    for(let i = 0; i < n; i++) {
-        if(i % 2 === 0) answer += "수";
-        else answer+= "박";
-    }
-    return answer;
-}
+// function solution(n) {
+//     let answer = '';
+//     for(let i = 0; i < n; i++) {
+//         if(i % 2 === 0) answer += "수";
+//         else answer+= "박";
+//     }
+//     return answer;
+// }
+
+const solution = n => "수박".repeat(n).slice(0,n);
