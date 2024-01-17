7f71e0f4e035d04ff3a8f956127fd0f686521e62
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-01-17 18:02:53 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,4 +1,10 @@
 function solution(s) {
-    return (s.length === 4 || s.length === 6) && /^\d+$/.test(s);
-    // test() 메서드는 주어진 문자열이 정규 표현식을 만족하는지 판별하고, 그 여부를 true 또는 false로 반환
+  let answer = parseInt(s);
+ 
+  if ((s.length === 4 || s.length === 6) && s == answer) {
+    return (answer = true);
+  } else {
+    return (answer = false);
+  }
+  return answer;
 }
