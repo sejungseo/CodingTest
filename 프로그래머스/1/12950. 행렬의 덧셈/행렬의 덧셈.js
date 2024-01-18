5a462ec8ee0b2454428c7015ce57ea4d093e6522
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-01-18 21:39:25 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.96 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,4 +1,10 @@
 function solution(arr1, arr2) {
-    return arr1.map((value1, i) =>
-                   value1.map((value2, j) => value2 + arr2[i][j]));
+    let answer = [[]];
+    for(let i = 0; i < arr1.length; i++) {
+        answer[i] = [];
+        for(let j = 0; j < arr1[i].length; j++) {
+            answer[i].push(arr1[i][j] + arr2[i][j]);
+        }
+    }
+    return answer;
 }
