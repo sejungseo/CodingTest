8abb0e588d85674f4fe05cda747615e96d0b4b34
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-01-23 15:38:00 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,8 +1,8 @@
 function solution(d, budget) {
     let answer = 0;
-    let sortedD = d.sort((a, b) => a - b);
-    for(let i = 0; i < sortedD.length; i++) {
-        budget -= sortedD[i];
+    d.sort((a, b) => a - b);
+    for(let i = 0; i < d.length; i++) {
+        budget -= d[i];
         if(budget < 0) {
             break;
         }
