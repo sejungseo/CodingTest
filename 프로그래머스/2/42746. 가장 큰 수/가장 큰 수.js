258e38b283e6110854996e061e1753c08baaaaac
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-01-30 15:31:38 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,7 +1,8 @@
 function solution(numbers) {
     let answer = numbers
-        .map((number) => number.toString())
-        .sort((a, b) => (b + a) - (a + b))
+        .map((number) => number.toString()) // 문자열 변환
+        .sort((a, b) => (b + a) - (a + b))  // 문자열 조합 비교 정렬
         .join('');
     return answer[0] === '0' ? '0' : answer;
+    // 가장 큰 수가 0일 경우 '000...'이 되므로 0 반환
 }
