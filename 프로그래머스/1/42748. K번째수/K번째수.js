455f684e1795f305835d9f4df6f439efdcbf4ede
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-01-30 14:58:09 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,14 +1,9 @@
 function solution(array, commands) {
     let answer = [];
-    let temp = [];
-    
-    for(let i = 0; i < commands.length; i++) {
-        const start = commands[i][0] - 1;
-        const end = commands[i][1];
-        
-        temp.push(array.slice(start, end).sort((a, b) => a - b));
-        console.log(temp)
-        answer.push(temp[i][commands[i][2] - 1]);
-    }
+    commands.forEach(([i, j, k]) => {
+        const slicedArr = array.slice(i - 1, j).sort((a, b) => a - b);
+        const number = slicedArr[k - 1]
+        answer.push(number);
+    })
     return answer;
 }
