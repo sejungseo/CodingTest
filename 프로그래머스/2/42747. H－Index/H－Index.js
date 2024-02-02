c1f5e74fe5e048198436f206e80cf11a1f59b0a0
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-02-02 16:04:14 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,10 +1,9 @@
 function solution(citations) {
+    let answer = 0;
     citations.sort((a, b) => b - a);
-    let h;
-    for(h = 0; h < citations.length; h++) {
-        if(citations[h] <= h) {
-            break;
-        }
+    for(let h = 0; h < citations.length; h++) {
+        if(citations[h] > h) answer++;
+        else break;
     }
-    return h;
+    return answer;
 }
