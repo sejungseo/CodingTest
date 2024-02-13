777ca1aedd5a34c671c2f29845c89639d2e804f2
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-02-13 22:40:23 +0900

    [level 2] Title: 주식가격, Time: 28.00 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,9 +1,9 @@
 function solution(prices) {
-    let answer = new Array(prices.length).fill(0);
-    for(let i = 0; i < prices.length; i++) {
-        for(let j = i + 1; j < prices.length; j++) {
-            answer[i]++;
-            if(prices[i] > prices[j]) {
+    let answer = new Array(prices.length).fill(0); // 결과 배열 초기화
+    for(let i = 0; i < prices.length; i++) { // 현재 가격
+        for(let j = i + 1; j < prices.length; j++) { // 비교 대상 가격
+            answer[i]++; // 각 초마다 시간 증가
+            if(prices[i] > prices[j]) { // 가격이 떨어지면 반복문 종료
                 break;
             }
         }
