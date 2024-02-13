d8e56ec2bb1910bee34ed0af322413a6367ad1ad
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-02-13 22:38:33 +0900

    [level 2] Title: 주식가격, Time: 5.03 ms, Memory: 42.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,12 @@
+function solution(prices) {
+    let answer = new Array(prices.length).fill(0);
+    for(let i = 0; i < prices.length; i++) {
+        for(let j = i + 1; j < prices.length; j++) {
+            answer[i]++;
+            if(prices[i] > prices[j]) {
+                break;
+            }
+        }
+    }
+    return answer;
+}
