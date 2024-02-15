ab726a83de3e7ede305f882a00d5fbbc2ef4ca3a
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-02-15 15:03:44 +0900

    [level 1] Title: 모의고사, Time: 3.12 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -5,14 +5,13 @@ function solution(answers) {
         pattern2 = [2, 1, 2, 3, 2, 4, 2, 5],
         pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    answers.forEach((answer, idx) => {
-        if(pattern1[idx % pattern1.length] === answer) person1++;
-        if(pattern2[idx % pattern2.length] === answer) person2++;
-        if(pattern3[idx % pattern3.length] === answer) person3++;
-    })
+    for(let i = 0; i < answers.length; i++) {
+        if(answers[i] === pattern1[i % pattern1.length]) person1++;
+        if(answers[i] === pattern2[i % pattern2.length]) person2++;
+        if(answers[i] === pattern3[i % pattern3.length]) person3++;
+    }
     
     const maxAnswer = Math.max(person1, person2, person3);
-    
     if(person1 === maxAnswer) answer.push(1);
     if(person2 === maxAnswer) answer.push(2);
     if(person3 === maxAnswer) answer.push(3);
