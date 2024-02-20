c0ee8e1b774ad89e9a75d541de7625eef414c6cb
sejung.seo <125885922+sejungseo@users.noreply.github.com>
2024-02-20 15:26:34 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,15 +1,20 @@
 function solution(operations) {
-    const heap = [];
-    const op = operations.map(operation => operation.split(' '));
+    const q = [];
     
-    op.forEach(num => {
-        if(num[0] === "I") {
-            heap.push(Number(num[1]))
-        } else {
-            const findValue = (num[1] === "1" ? Math.max : Math.min)(...heap);
-            const delIdx = heap.indexOf(findValue);
-            heap.splice(delIdx, 1);
+    for(let i = 0; i < operations.length; i ++){
+        const [command, num] = operations[i].split(" ");
+        if(command === "I") {
+            q.push(num);
+        } else if(q.length !== 0){
+            if(num === "1"){
+                q.sort((a, b) => a - b);
+                q.pop();
+            } else {
+                q.sort((a, b) => b - a);
+                q.pop();
+            }
         }
-    })
-    return heap.length ? [Math.max(...heap), Math.min(...heap)] : [0, 0];
+    }
+    q.sort((a, b) => a - b);
+    return q.length === 0 ? [0,0] : [+q[q.length - 1], +q[0]];
 }
